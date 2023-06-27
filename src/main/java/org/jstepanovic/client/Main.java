3bf568a52df6276e9d4f76d4618c4110d08d8c2f
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-06-27 22:24:56 +0200

    removed emply lines in client Main

--- src/main/java/org/jstepanovic/client/Main.java
+++ src/main/java/org/jstepanovic/client/Main.java
@@ -2,9 +2,6 @@ package org.jstepanovic.client;
 
 import org.jstepanovic.client.service.Client;
 
-
-
-
 public class Main {
     public static void main(String[] args) {
         Client client = new Client();
