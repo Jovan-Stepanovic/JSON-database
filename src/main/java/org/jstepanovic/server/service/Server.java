da64a0c18658e9d086fbfe6139daedc3c202697e
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-07-25 10:28:18 +0200

    isServerRunning is thread safe, small refactoring

--- src/main/java/org/jstepanovic/server/service/Server.java
+++ src/main/java/org/jstepanovic/server/service/Server.java
@@ -8,6 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.jstepanovic.commons.Constants.SERVER_ADDRESS;
 import static org.jstepanovic.commons.Constants.SERVER_PORT;
@@ -17,11 +18,11 @@ public enum Server {
     INSTANCE;
 
     private final int numberOfProcessors = Runtime.getRuntime().availableProcessors();
-    private final ExecutorService executorService = Executors.newFixedThreadPool(numberOfProcessors);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(numberOfProcessors - 1);
 
     private final Repository repository = new Repository();
 
-    private boolean isServerRunning;
+    private final AtomicBoolean isServerRunning = new AtomicBoolean(false);
 
     private ServerSocket serverSocket;
 
@@ -29,7 +30,7 @@ public enum Server {
         try {
             serverSocket = new ServerSocket(SERVER_PORT, 50, InetAddress.getByName(SERVER_ADDRESS));
 
-            isServerRunning = true;
+            isServerRunning.set(true);
             System.out.println("Server is running");
 
             executorService.submit(new Session(serverSocket.accept(), repository));
@@ -40,8 +41,7 @@ public enum Server {
                     System.out.println("started communication with the new client");
                     executorService.submit(new Session(clientSocket, repository));
                 } catch (SocketException e) {
-                    isServerRunning = false;
-                    System.out.println("Server stopped");
+                    stop();
                     break;
                 }
             }
@@ -52,8 +52,15 @@ public enum Server {
     }
 
     public synchronized void stop() {
+
+        if (!isServerRunning.get()) {
+            System.out.println("Server is not running");
+            return;
+        }
+
         try {
             executorService.shutdown();
+            isServerRunning.set(false);
             executorService.awaitTermination(200, TimeUnit.MILLISECONDS);
             System.out.println("Server is stopping....");
             serverSocket.close();
@@ -63,7 +70,7 @@ public enum Server {
     }
 
     public boolean isRunning() {
-        return isServerRunning;
+        return isServerRunning.get();
     }
 
 }
